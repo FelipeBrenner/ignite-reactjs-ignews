16fe726a8eee92f2d2956724d1da2adc50e19f28
Felipe Brenner <felipeobrenner@hotmail.com>
2021-04-13 21:29:44 -0300

    Defined Styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,14 @@
+import Head from 'next/head'
+
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <Head>
+        <title>Inicio | ig.news</title>
+      </Head>
+      <h1>
+        Hello World
+      </h1>
+    </>
   )
 }
