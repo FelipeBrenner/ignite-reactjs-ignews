1867bce411297337fc1b3c252d7d4f8e9f89a37e
Felipe Brenner <felipeobrenner@hotmail.com>
2021-05-22 10:14:18 -0300

    Validando assinatura ativa

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -42,6 +42,15 @@ export const getServerSideProps: GetServerSideProps = async ({
   const session = await getSession({ req });
   const { slug } = params;
 
+  if (!session.activeSubscription) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
   const prismic = getPrismicClient(req);
 
   const response = await prismic.getByUID("post", String(slug), {});
